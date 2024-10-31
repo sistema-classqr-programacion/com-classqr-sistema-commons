aba84417c148005c73cdb5d13f8bdc6a835fa8dd
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-10-31 14:34:56 -0500

    Se agregan cambios en el mapper

--- src/main/java/com/classqr/sistema/commons/util/mapper/AsistenciaMapper.java
+++ src/main/java/com/classqr/sistema/commons/util/mapper/AsistenciaMapper.java
@@ -5,6 +5,8 @@ import com.classqr.sistema.commons.entity.AsistenciaEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import java.util.List;
 
@@ -15,10 +17,18 @@ public interface AsistenciaMapper {
 
     AsistenciaEntity dtoToEntity(AsistenciaDTO dto);
 
-    List<AsistenciaDTO> listEntityTolistDto(List<AsistenciaEntity> entity);
+    List<AsistenciaDTO> listEntityToListDto(List<AsistenciaEntity> entity);
 
     List<AsistenciaEntity> listDtoToListEntity(List<AsistenciaDTO> dto);
 
+    default Page<AsistenciaDTO> pageEntityToPageDto(Page<AsistenciaEntity> entities) {
+        List<AsistenciaDTO> dtos = listEntityToListDto(entities.getContent());
+        return new PageImpl<>(dtos, entities.getPageable(), entities.getTotalElements());
+    }
 
+    default Page<AsistenciaEntity> pageDtoToPageEntity(Page<AsistenciaDTO> dtos) {
+        List<AsistenciaEntity> entities = listDtoToListEntity(dtos.getContent());
+        return new PageImpl<>(entities, dtos.getPageable(), dtos.getTotalElements());
+    }
 
 }
