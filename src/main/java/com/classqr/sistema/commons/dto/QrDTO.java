f7742912eaa84d645923967774d5ec000c043198
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-11-25 01:32:31 -0500

    Se agrega anotacion

--- src/main/java/com/classqr/sistema/commons/dto/QrDTO.java
+++ src/main/java/com/classqr/sistema/commons/dto/QrDTO.java
@@ -7,11 +7,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class QrDTO {
 
     private String codigoQr;
