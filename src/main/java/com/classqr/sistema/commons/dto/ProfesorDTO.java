6d8237189c4e992bada2bc3d604225477d20f280
MariaFernanda1818 <mariafvn0127@gmail.com>
2024-11-25 01:41:24 -0500

    Se agrega anotacion

--- src/main/java/com/classqr/sistema/commons/dto/ProfesorDTO.java
+++ src/main/java/com/classqr/sistema/commons/dto/ProfesorDTO.java
@@ -3,9 +3,11 @@ package com.classqr.sistema.commons.dto;
 import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProfesorDTO {
 
     private String codigoProfesor;
